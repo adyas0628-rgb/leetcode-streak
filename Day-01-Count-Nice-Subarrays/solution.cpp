d1a0fe1b95b977560554661c1d76ced2762c7341
Adya28 <adyas0628@gmail.com>
2026-04-07 23:45:50 +0530

    Create solution.cpp

---
+++ Day-01-Count-Nice-Subarrays/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int count_Subarrays(vector<int>& nums, int k){
+        if(k<0)return 0;
+        int l=0,r=0,cnt=0,sum=0;
+        while(r<nums.size()){
+            sum += nums[r]%2;
+            while(sum>k){
+                sum-=nums[l]%2;                           
+                l++;
+                
+            }
+            cnt+= (r-l+1);
+            r++;
+        }
+        return cnt;
+    }
+    int numberOfOddSubarrays(vector<int>& nums, int k) {
+        //your code goes here
+        return count_Subarrays(nums, k)- count_Subarrays(nums, k-1);
+    }
+};
